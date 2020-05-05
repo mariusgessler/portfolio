518ed2f9581005fd254607d3779ca9c8f3f520b5
mariusgessler <marius.gessler@gmail.com>
2020-05-05 12:59:49 +0200

    cleaned up code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'gatsby';
-import SEO from '../components/nanos/seo';
+import SEO from '../utilis/seo';
 import Hero from '../components/hero';
 import Work from '../components/work';
 import ScrollProgress from '../components/nanos/scrollprogress';
@@ -9,46 +7,17 @@ import Footer from '../components/footer';
 import About from '../components/about';
 import '../css/styles.scss';
 
-const Container = styled.div`
-padding: 1.2em;
-  @media (min-width: 700px) {
-    padding: 0.7em 2em;
-  }
-  @media (min-width: 850px) {
-  }
-  @media (min-width: 1100px) {
-    width: calc(860px - 1.4rem);
-    margin: 0 auto;
-  }`;
-
-const ActionCallContainer = styled.div``;
-
-const ActionCall = styled.p`
-text-align: center;
-margin: 70px auto;
-font-weight: 100;
-font-size: 1.3rem;
-  @media (min-width: 700px){
-    font-size: 1rem;
-  }`;
-
 const IndexPage = () => (
-  <>
-    <SEO title="Portfolio" />
-    <ScrollProgress />
-
-    <div className="container">
-      <div className="container-fluid">
+  <div className="container">
+    <div className="container-fluid">
+      <SEO title="Portfolio" />
+      <ScrollProgress />
       <Hero />
       <Work />
       <About />
       <Footer />
-
-      </div>
     </div>
-
-
-  </>
+  </div>
 );
 
 export default IndexPage;
