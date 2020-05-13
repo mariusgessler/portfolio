e42a17720b121bd7f3491caf2c802e932b4c42ba
mariusgessler <marius.gessler@gmail.com>
2020-05-13 21:07:17 +0200

    updated hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,27 +3,31 @@ import portrait from '../assets/illustrations/portrait.svg';
 
 const Hero = () => (
   <div className="section section--hero">
-    <div className="row center-xs middle-sm between-sm">
-      <div className="col col-xs-12 col-sm-4">
-        <div className="introduction text__wrapper">
-          <h1>Hi, I&apos;m Marius.</h1>
-          <h2>
-            I&apos;m a developer who enjoys working on meaningful projects with some nice people,
-            currently at
-            <a
-              href="https://hiber.global/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {' '}
-              Hiber.
-            </a>
-          </h2>
-        </div>
-      </div>
-      <div className="col col-xs-12 col-sm-4">
-        <div className="img__wrapper fade-in">
-          <img src={portrait} alt="Imagetrace of Marius" />
+    <div className="container">
+      <div className="container-fluid">
+        <div className="row center-xs between-sm middle-sm">
+          <div className="col col-xs-12 col-sm-4">
+            <div className="introduction text__wrapper">
+              <h1>Hi, I&apos;m Marius.</h1>
+              <h2>
+                I&apos;m a developer who enjoys working on meaningful projects 
+                with some nice people, currently at
+                <a
+                  href="https://hiber.global/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {' '}
+                  Hiber.
+                </a>
+              </h2>
+            </div>
+          </div>
+          <div className="col col-xs-12 col-sm-4">
+            <div className="img__wrapper fade-in">
+              <img src={portrait} alt="Imagetrace of Marius" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
