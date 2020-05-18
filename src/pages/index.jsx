c90d161cf7b9d0f61bf08e182c7e11f09ad2e80e
mariusgessler <marius.gessler@gmail.com>
2020-05-18 23:14:47 +0200

    Add console greeting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,16 +7,20 @@ import Footer from '../components/footer';
 import About from '../components/about';
 import '../css/styles.scss';
 
-const IndexPage = () => (
-  <>
-    <SEO title="Portfolio" />
-    <ScrollProgress />
-    <Hero />
-    <Work />
-    <About />
-    <Footer />
-  </>
+const IndexPage = () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hi there!${String.fromCodePoint(0x1F44B)}`);
 
-);
+  return (
+    <>
+      <SEO title="Portfolio" />
+      <ScrollProgress />
+      <Hero />
+      <Work />
+      <About />
+      <Footer />
+    </>
+  );
+};
 
 export default IndexPage;
